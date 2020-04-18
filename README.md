9ac9aadf3da5a527d57ad91bea7a70f6684d9df2
Kerolos Zaki <kerolos4zaki@gmail.com>
2020-04-18 15:43:45 +0200

    0.1.0

---
+++ README.md
@@ -0,0 +1,225 @@
+![npm (tag)](https://img.shields.io/npm/v/flcss/latest)
+![npm](https://img.shields.io/npm/dm/flcss)
+
+# FLCSS
+
+A very bad implementation of CSS-in-JS.
+
+But it supports all of the things like selectors, pseudo-classes, pseudo-elements, attributes, vendor prefixes.
+
+It also has some custom features like extending but properly is one of the slowest implementations of CSS-IN-JS.
+
+but yeah, because of all that it's not the fastest CSS-in-JS library out there.
+
+## Installation
+
+npm install --save-dev flcss`
+
+## Usage
+
+flcss will work with any web framework that allows you set elements' class name.
+
+```jsx
+import React from 'react';
+import { createStyle } from 'flcss';
+
+const Box = () => {
+  return <div className={ styles.red }/>;
+};
+
+const styles = createStyle({
+  red: {
+    padding: '15px',
+    backgroundColor: 'red'
+  }
+});
+
+export default Box;
+```
+
+### Extending
+
+```js
+const styles = createStyle({
+  red: {
+    padding: '15px',
+    backgroundColor: 'red'
+  },
+  blue: {
+    extend: 'red',
+    backgroundColor: 'blue'
+  }
+});
+```
+
+### Pseudo-classes and Pseudo-elements
+
+```js
+const styles = createStyle({
+  red: {
+    padding: '15px',
+    backgroundColor: 'red',
+
+    ':hover': {
+      backgroundColor: 'black',
+    }
+  }
+});
+```
+
+```js
+const styles = createStyle({
+  input: {
+    fontSize: '12px',
+    color: 'black',
+
+    '::placeholder': {
+      color: 'grey'
+    }
+  }
+});
+```
+
+### Media Queries
+
+```js
+const styles = createStyle({
+  red: {
+    padding: '15px',
+    backgroundColor: 'red',
+
+    '@media screen and (max-width: 1080px)': {
+      padding: '8px'
+    }
+  }
+});
+```
+
+### Attributes
+
+```js
+const styles = createStyle({
+  button: {
+    padding: '15px',
+    cursor: 'pointer',
+    backgroundColor: 'black',
+
+    '[enabled="false"]': {
+      cursor: 'default',
+      pointerEvents: 'none',
+      backgroundColor: 'grey'
+    }
+  }
+});
+```
+
+### Child Selectors
+
+```js
+const styles = createStyle({
+  red: {
+    padding: '15px',
+    backgroundColor: 'red',
+
+    '> div': {
+      padding: '25px',
+      backgroundColor: 'green',
+    }
+  }
+});
+```
+
+
+### Animations
+
+```js
+const boxAnimation = createAnimation({
+  duration: '0.5s',
+  timingFunction: 'cubic-bezier(0.18, 0.89, 0.32, 1.28)',
+  fillMode: 'forwards',
+  iterationCount: '1',
+  keyframes: `100% {
+    margin: 5px
+  }
+  100% {
+    margin: 10px
+  }`
+});
+
+const styles = createStyle({
+  box: {
+    animation: boxAnimation
+  }
+})
+```
+
+```js
+const hoverAnimation = createAnimation({
+  options: {
+    returnNameOnly: true
+  },
+  keyframes: `0% {
+    transform: translateY(-10px);
+  }
+  50% {
+    transform: translateY(-5px);
+  }
+  100% {
+    transform: translateY(-10px);
+  }`
+});
+
+const floatAnimation = createAnimation({
+  options: {
+    returnNameOnly: true
+  },
+  keyframes: `100% {
+    transform: translateY(-10px);
+  }`
+});
+
+const styles = createStyle({
+  box: {
+      animationName: `${floatAnimation}, ${hoverAnimation}`,
+
+      animationDuration: '.3s, 1.5s',
+      animationDelay: '0s, .3s',
+      animationTimingFunction: 'ease-out, ease-in-out',
+      animationIterationCount: '1, infinite',
+      animationFillMode: 'forwards',
+      animationDirection: 'normal, alternate'
+    }
+  }
+})
+```
+
+###  Other Examples
+
+```js
+const styles = createStyle({
+  red: {
+    padding: '15px',
+    backgroundColor: 'red',
+
+    '> :hover': {
+      padding: '15px',
+      backgroundColor: 'yellow',
+    }
+  }
+});
+```
+
+```js
+const styles = createStyle({
+  container: {
+    width: '100%',
+
+    ' .red-box': {
+      padding: '15px',
+      backgroundColor: 'red'
+    }
+  }
+});
+```
+
+Basically, do anything you want, it will probably work, and if it didn't then open an issue and request it, and we'll add it.
---
+++ src/index.js
@@ -0,0 +1,232 @@
+/* eslint-disable security/detect-object-injection */
+
+const camelRegex = /[A-Z]/g;
+const selectorsRegex = /^\s|^>|^\+|^~/;
+const classRegex = /^[a-z]|^-/i;
+
+/** Creates a stylesheet,
+* the returned object can be used to style elements via class names property
+* @param { T } styles
+* @template T
+* @returns { T }
+* @example
+*
+* const styles = createStyle({
+*    bigBlue: {
+*    color: 'blue',
+*    fontWeight: 'bold',
+*    fontSize: '12px'
+*  }});
+*
+*  return <div className={styles.bigBlue}/>;
+*/
+export function createStyle(styles)
+{
+  const newStyles = {};
+  const directory = {};
+  
+  // loop the root object
+  for (const key in styles)
+  {
+    const obj = styles[key];
+
+    if (typeof obj !== 'object')
+      continue;
+
+    const { className, css }  = handleStyle(key, obj, newStyles, directory);
+
+    newStyles[key] = className;
+    directory[key] = css;
+  }
+
+  // append the stylesheet element to dom
+  appendToDOM(Object.values(directory).join(''));
+  
+  // return the new class names to the user
+  return newStyles;
+}
+
+/** Creates a CSS animation
+* @param { { keyframes: string, options: { returnNameOnly: boolean }, duration: string, timingFunction, delay: string, iterationCount, direction, fillMode } } animation
+*
+* @example
+*
+* const animationA = createAnimation({
+*    duration: '4s',
+*    keyframes: `
+*    from: { background-color: red; }
+*    to: { background-color: yellow; }
+*    `,
+* });
+*
+* const styles = createStyle({
+*    someMovingThingy: {
+*    width: '35%',
+*    height: '15%',
+*    animation: animationA
+*  }});
+*
+*  return <div className={styles.someMovingThingy}/>;
+*/
+export function createAnimation(animation)
+{
+  // make sure that no strings equal 'undefined'
+  // set default values
+
+  animation.options = animation.options || {};
+
+  animation.timingFunction = animation.timingFunction || 'ease';
+  animation.delay = animation.delay || '0s';
+  animation.iterationCount = animation.iterationCount || '1';
+  animation.duration = animation.duration || '1s';
+  animation.direction = animation.direction || 'normal';
+  animation.fillMode = animation.fillMode || 'none';
+
+  // generate a random name for the animation
+  const animationName = `flcss-animation-${Math.random().toString(36).substr(2, 7)}`;
+
+  // the animation's css string
+  const css = `@keyframes ${animationName}{${animation.keyframes}}`;
+
+  // append the css string to DOM
+  appendToDOM(css);
+
+  // return a string the developers can use with createStyle()
+  if (animation.options.returnNameOnly)
+    return animationName;
+  else
+    return `${animationName} ${animation.duration} ${animation.timingFunction} ${animation.delay} ${animation.iterationCount} ${animation.direction} ${animation.fillMode}`.replace(/\s+/g, ' ');
+}
+
+/** @param { string } key
+* @param { {} } obj
+* @param { {} } rootDirectory
+* @param { {} } rootStylesheet
+* @param { string } nest
+*/
+function handleStyle(key, obj, rootDirectory, rootStylesheet, nest)
+{
+  let css = '';
+  let additionalCss = '';
+
+  // the values of the rules
+  const values = Object.values(obj);
+
+  // create a class name using the original class name as a prefix and a random characters
+  const className = `flcss-${key}-${Math.random().toString(36).substr(2, 7)}`;
+
+  // loop though the rules
+  Object.keys(obj).forEach((rule, i) =>
+  {
+    // extent support
+    if (rule === 'extend' && rootStylesheet)
+    {
+      if (!Array.isArray(values[i]))
+        values[i] = [ values[i] ];
+      
+      values[i].forEach((v) =>
+      {
+        const originalClassnames = rootDirectory[v];
+
+        // eslint-disable-next-line security/detect-non-literal-regexp
+        rootStylesheet[v] = rootStylesheet[v].replace(new RegExp(`${originalClassnames}.*?{`, 'g'), (s) =>
+        {
+          const add = s.replace(originalClassnames, '').replace('{', '');
+          
+          return `${originalClassnames}${add}, .${className}${add}{`;
+        });
+      });
+    }
+    else if (rule.includes('%this'))
+    {
+      const nextParent = `${rule.replace('%this', `.${className}`)}`;
+
+      additionalCss = additionalCss + `${nextParent}${handleStyle(rule, values[i], rootDirectory, rootStylesheet, nextParent)}`;
+    }
+    else if (
+      // it must be an attribute
+      rule.startsWith('[') ||
+      // it must be an class
+      rule.startsWith('.') ||
+      // it must be an tag
+      rule.startsWith('#') ||
+      // it must be pseudo-class or pseudo-element
+      rule.startsWith(':') ||
+      rule.startsWith('::') ||
+      // other selectors
+      selectorsRegex.test(rule)
+    )
+    {
+      if (typeof values[i] === 'object')
+      {
+        let nextParent = '';
+
+        if (nest)
+          nextParent = `${nest}${rule}`;
+        else
+          nextParent = `${className}${rule}`;
+
+        additionalCss = additionalCss + `.${nextParent}${handleStyle(rule, values[i], rootDirectory, rootStylesheet, nextParent)}`;
+      }
+    }
+    // at-rule selector
+    else if (rule.startsWith('@'))
+    {
+      const nextParent = `${className}`;
+      
+      additionalCss = additionalCss + `${rule}{.${nextParent}${handleStyle(rule, values[i], rootDirectory, rootStylesheet, nextParent)}}`;
+    }
+    // class selector
+    else if (classRegex.test(rule))
+    {
+      if (typeof values[i] === 'string' || typeof values[i] === 'number')
+      {
+        // if first char is capitalized
+        if (rule.substring(0, 1) === rule.substring(0, 1).toUpperCase())
+          rule = `-${rule.substring(0, 1).toLowerCase()}${rule.substring(1)}`;
+
+        // transform rule name from camelCase to no-caps
+        const noCaps = rule.replace(camelRegex, (c) => `-${c.toLowerCase()}`);
+    
+        // add the rule name and the value to the class css string
+        css = css + `${noCaps}: ${values[i]};`;
+      }
+    }
+    else
+    {
+      console.warn(`flcss warning: "${rule}" is unsupported rule and it was ignored`);
+    }
+  });
+
+  if (!nest)
+  {
+    return {
+      className: className,
+      css: `.${className}{${css}}${additionalCss}`
+    };
+  }
+  else
+  {
+    return `{${css}}${additionalCss}`;
+  }
+}
+
+/** @param { string } css
+*/
+function appendToDOM(css)
+{
+  // if document is not null
+  if (typeof document !== 'undefined')
+  {
+    const stylesheetElement = document.createElement('style');
+
+    // stylesheet is css
+    stylesheetElement.type = 'text/css';
+
+    // append the css string to the stylesheet element
+    stylesheetElement.appendChild(document.createTextNode(css));
+
+    // append the element to head
+    document.head.appendChild(stylesheetElement);
+  }
+}
